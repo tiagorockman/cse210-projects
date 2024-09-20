bd37ba59f4eba7302d653377dfc43a1f4e8a4a84
Tiago Neves <tiagorokman@gmail.com>
2024-09-20 19:17:37 -0300

    Conditional and Loops

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Inform your grade percentage!");
+        string stringGrade = Console.ReadLine();
+        int grade = Int32.Parse(stringGrade);
+
+        (bool aproved, string letter) = StudentStatusDefine(grade);
+
+        Console.WriteLine($"Your grade is {letter}.");
+        if(aproved)
+            Console.WriteLine($"Congratulations! You're approved!");
+        else
+            Console.WriteLine($"Keep Working! You can do a better job next time.");
+
+
+
+    }
+
+    static (bool,string) StudentStatusDefine(int grade){
+        string letter = "";
+        string letterSignal = "";
+        if(grade >=90){
+            letter = "A";
+            letterSignal= GetLetterSignal(grade);
+            if(letterSignal != "+")
+               letter = letter + letterSignal;
+            return (true, letter);
+        }else if (grade >=80){
+            letter = "B";
+            letterSignal= GetLetterSignal(grade);
+            return (true, letter + letterSignal);
+        }else if(grade >= 70){
+            letter = "C";
+            letterSignal= GetLetterSignal(grade);
+            return (true, letter + letterSignal);
+        }else if(grade >=60){
+            letter = "D";
+            letterSignal= GetLetterSignal(grade);
+            return (false, letter + letterSignal);
+        }else{
+            return (false, "F");
+        }
+    }
+
+    static string GetLetterSignal(int grade){
+        int remainder = grade % 10;
+        if(remainder >= 7)
+            return "+";
+        else if (remainder >3)
+            return "-";
+        else
+            return "";
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool keepPlaying = true;
+
+        while(keepPlaying){
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1,100);
+            int guessNumber = 0;
+            int guessCounter = 1;
+            do {
+                Console.WriteLine("\nWhat is your guess?");
+                guessNumber = int.Parse(Console.ReadLine());
+                if(guessNumber > magicNumber){
+                    Console.WriteLine("Lower");
+                    guessCounter++;
+                }else if(guessNumber < magicNumber){
+                    Console.WriteLine("Higher");
+                    guessCounter++;
+                }else{
+                    Console.WriteLine("\nYou guessed it!");
+                    Console.WriteLine($"You spent {guessCounter} times to guess it!");
+
+                    Console.WriteLine("\nDo you want to keep playing? 1 - [YES] 0 - [NO] ");
+                    int response = int.Parse(Console.ReadLine());
+                    keepPlaying = response == 1 ? true : false; 
+                }
+            } while (guessNumber != magicNumber);
+        }      
+             
     }
 }
